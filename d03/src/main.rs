c986bb62c065fa98b561801d4db7a7c4e969bd71
Nighthawk <luks.schroeter@gmail.com>
2024-08-06 13:03:25 +0200

    1303

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -1,7 +1,7 @@
 
 use std::{fs::read_to_string};
 fn main() {
-    let input: String = read_to_string("demo.txt").unwrap();
+    let input: String = read_to_string("input.txt").unwrap();
     let tokens: Vec<String> = input.split('\n').map(String::from).collect();
 
     let daten:Vec<Vec<(i32,i32)>> = tokens.iter().filter(|x:&&String| !x.is_empty()).map(|x|x.split(",").map(|y|{
@@ -19,13 +19,9 @@ let score:i32 = daten.iter().map(|x|{
     let e2 =x[1];
 
     // ist e1 in e2 enthalten
-    if (e1.0 >= e2.0 && e1.1 <= e2.1) {
+    if (e1.0 >= e2.0 && e1.1 <= e2.1) || (e1.0 <= e2.0 && e1.1 >= e2.1){
         return 1;
     }
-    else if e1.0 <= e2.0 && e1.1 >= e2.1  {
-        return 1;
-    }
-
     0  
 
 }).sum();
@@ -35,24 +31,32 @@ println!("{}", score);
 // Part2
 // 5 7
 // 7 9
-
-// 7 9
-// 5 7
+ 
+// 2 4 
+// 6 8
 let score2:i32 = daten.iter().map(|x|{
     let e1 = x[0];
     let e2 =x[1];
 
-    if e1.0 >= e2.0{
+    if e1.0 <= e2.1 && e2.0 <= e1.1 {
         return 1;
     }
-    else if  {
-        
-    }
-   
    0
     
 
 }).sum();
 println!("{}", score2);
+
+let e1 = (2,8);
+let e2 =(3,7);
+
+if e1.0 >= e2.0 || e1.1 <= e2.1  {
+
+    println!("Bumm");
+}
+
+
+
+
 
 }
